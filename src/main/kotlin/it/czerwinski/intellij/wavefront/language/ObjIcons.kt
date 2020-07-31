07a5a23bf66f526fb8d2b5b2cbeb144b1523c80e
Slawomir Czerwinski <slawomir@czerwinski.it>
2020-07-31 18:31:55 +0100

    Registered Wavefront OBJ language and file type

---
+++ src/main/kotlin/it/czerwinski/intellij/wavefront/language/ObjIcons.kt
@@ -0,0 +1,24 @@
+/*
+ * Copyright 2020 Slawomir Czerwinski
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+@file:JvmName("WavefrontObjIcons")
+
+package it.czerwinski.intellij.wavefront.language
+
+import com.intellij.openapi.util.IconLoader
+import javax.swing.Icon
+
+val OBJ_FILE_ICON: Icon = IconLoader.getIcon("/icons/objFileIcon.svg")

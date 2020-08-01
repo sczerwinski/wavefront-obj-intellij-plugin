e351cdd708f57e4f9bbabab8fb1d5d17d285279a
Slawomir Czerwinski <slawomir@czerwinski.it>
2020-08-01 17:17:13 +0100

    Added icons for Structure View

--- src/main/kotlin/it/czerwinski/intellij/wavefront/language/ObjIcons.kt
+++ src/main/kotlin/it/czerwinski/intellij/wavefront/language/ObjIcons.kt
@@ -22,3 +22,23 @@ import com.intellij.openapi.util.IconLoader
 import javax.swing.Icon
 
 val OBJ_FILE_ICON: Icon = IconLoader.getIcon("/icons/objFileIcon.svg")
+
+val OBJ_ERROR_ICON: Icon = IconLoader.getIcon("/icons/objErrorIcon.svg")
+
+val OBJ_OBJECT_ICON: Icon = IconLoader.getIcon("/icons/objObjectIcon.svg")
+val OBJ_GROUP_ICON: Icon = IconLoader.getIcon("/icons/objGroupIcon.svg")
+
+val OBJ_VERTEX_ICON: Icon = IconLoader.getIcon("/icons/objVertexIcon.svg")
+val OBJ_TEXTURE_COORDINATES_ICON: Icon = IconLoader.getIcon("/icons/objTextureCoordinatesIcon.svg")
+val OBJ_VERTEX_NORMAL_ICON: Icon = IconLoader.getIcon("/icons/objVertexNormalIcon.svg")
+
+val OBJ_FACE_TRIANGLE_ICON: Icon = IconLoader.getIcon("/icons/objFaceTriangleIcon.svg")
+val OBJ_FACE_QUAD_ICON: Icon = IconLoader.getIcon("/icons/objFaceQuadIcon.svg")
+val OBJ_FACE_POLYGON_ICON: Icon = IconLoader.getIcon("/icons/objFacePolygonIcon.svg")
+val OBJ_LINE_ICON: Icon = IconLoader.getIcon("/icons/objLineIcon.svg")
+val OBJ_POINT_ICON: Icon = IconLoader.getIcon("/icons/objPointIcon.svg")
+
+val OBJ_FACE_VERTEX_ICON: Icon = IconLoader.getIcon("/icons/objFaceVertexIcon.svg")
+
+val OBJ_SMOOTH_SHADING_ON_ICON: Icon = IconLoader.getIcon("/icons/objSmoothShadingOnIcon.svg")
+val OBJ_SMOOTH_SHADING_OFF_ICON: Icon = IconLoader.getIcon("/icons/objSmoothShadingOffIcon.svg")

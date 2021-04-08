e45fc861906d065b0675a401e679db1e8d485394
Slawomir Czerwinski <slawomir@czerwinski.it>
2021-04-08 21:15:10 +0100

    Upgrade Glimpse to 1.0.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -33,9 +33,9 @@ repositories {
 
 dependencies {
     api(fileTree(mapOf("dir" to "jogamp-2.4-SNAPSHOT", "include" to listOf("*.jar"))))
-    api("graphics.glimpse:glimpse-core:1.0.0-RC1")
-    api("graphics.glimpse:glimpse-ui:1.0.0-RC1")
-    kapt("graphics.glimpse:glimpse-processor-java:1.0.0-RC1")
+    api("graphics.glimpse:glimpse-core:1.0.0")
+    api("graphics.glimpse:glimpse-ui:1.0.0")
+    kapt("graphics.glimpse:glimpse-processor-java:1.0.0")
     detektPlugins("io.gitlab.arturbosch.detekt:detekt-formatting:1.16.0")
 }
 

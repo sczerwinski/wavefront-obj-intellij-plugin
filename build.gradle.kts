76870287f307a4e33a61c0f3db9cdc301794b3cd
Sławomir Czerwiński <slawomir@czerwinski.it>
2022-03-13 19:50:53 +0000

    Update dependencies (#254)
    * Upgrade dependencies
    * Add support for IntelliJ platform 2022.1

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,9 +12,9 @@ plugins {
     // Kotlin support
     id("org.jetbrains.kotlin.jvm") version "1.6.10"
     // Kotlin Symbol Processing - read more: https://github.com/google/ksp
-    id("com.google.devtools.ksp") version "1.6.10-1.0.2"
+    id("com.google.devtools.ksp") version "1.6.10-1.0.4"
     // gradle-intellij-plugin - read more: https://github.com/JetBrains/gradle-intellij-plugin
-    id("org.jetbrains.intellij") version "1.3.1"
+    id("org.jetbrains.intellij") version "1.4.0"
     // gradle-grammarkit-plugin - read more: https://github.com/JetBrains/gradle-grammar-kit-plugin
     id("org.jetbrains.grammarkit") version "2021.2.1"
     // gradle-changelog-plugin - read more: https://github.com/JetBrains/gradle-changelog-plugin

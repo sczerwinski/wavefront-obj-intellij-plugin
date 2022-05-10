ebad93828c25dd671d6569d1f9faf0a36fe8568c
Sławomir Czerwiński <slawomir@czerwinski.it>
2022-05-11 00:16:21 +0100

    Implement new file actions (#287)

---
+++ src/main/kotlin/it/czerwinski/intellij/wavefront/templates/MtlCreateFromTemplateHandler.kt
@@ -0,0 +1,24 @@
+/*
+ * Copyright 2020-2022 Slawomir Czerwinski
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package it.czerwinski.intellij.wavefront.templates
+
+import com.intellij.ide.fileTemplates.DefaultCreateFromTemplateHandler
+import com.intellij.ide.fileTemplates.FileTemplate
+import it.czerwinski.intellij.wavefront.lang.MtlFileType
+
+class MtlCreateFromTemplateHandler : DefaultCreateFromTemplateHandler() {
+
+    override fun handlesTemplate(template: FileTemplate): Boolean =
+        template.isTemplateOfType(MtlFileType)
+}
